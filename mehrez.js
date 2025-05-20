623578e43c83dcd95af7bec904afa6d515eaeb7a
shplax codes <117587733+avivR15@users.noreply.github.com>
2025-05-21 00:58:35 +0300

    Update mehrez.js

--- mehrez.js
+++ mehrez.js
@@ -1,1 +1,22 @@
+//שינוי שפה
+let currentLang = "en";
 
+function toggleLanguage() {
+    currentLang = currentLang === "en" ? "he" : "en";
+
+    const translatables = document.querySelectorAll("[data-en][data-he]");
+    translatables.forEach(el => {
+        el.textContent = el.dataset[currentLang];
+    });
+
+    // עדכון הטקסט של כפתור השפה
+    const langBtn = document.querySelector(".langBtn");
+    langBtn.textContent = currentLang === "en" ? "עברית" : "English";
+}
+
+
+//החלפה למצב בהירות
+function darkModeSwitch() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
